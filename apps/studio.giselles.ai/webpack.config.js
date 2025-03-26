95be2d898999ab4b1bd8417edf5c9c74d4a410ff
kaochannel154 <kaochannel154@gmail.com>
2025-03-26 13:10:58 +0900

    fix: add handlebars-loader and webpack config to resolve build issues

---
+++ apps/studio.giselles.ai/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.handlebars$/,
+        loader: 'handlebars-loader',
+        options: {
+          helperDirs: [path.join(__dirname, 'src/helpers')],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.handlebars'],
+  },
+}; 
