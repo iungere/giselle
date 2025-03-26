38330f10391dacfc928780add8d1f1d5e781cfd1
kaochannel154 <kaochannel154@gmail.com>
2025-03-26 13:43:47 +0900

    fix: apply biome formatting to webpack.config.js

--- apps/studio.giselles.ai/webpack.config.js
+++ apps/studio.giselles.ai/webpack.config.js
@@ -1,18 +1,18 @@
 const path = require("node:path");
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.handlebars$/,
-        loader: "handlebars-loader",
-        options: {
-          helperDirs: [path.join(__dirname, "src/helpers")],
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".ts", ".handlebars"],
-  },
-}; 
+	module: {
+		rules: [
+			{
+				test: /\.handlebars$/,
+				loader: "handlebars-loader",
+				options: {
+					helperDirs: [path.join(__dirname, "src/helpers")],
+				},
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".ts", ".handlebars"],
+	},
+};
